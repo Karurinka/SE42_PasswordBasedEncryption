32b7484eca1b3a6754d00331d221b970f4a3161b
Karurinka <michellebroens96@gmail.com>
2018-06-19 15:40:36 +0200

    Test class added

---
+++ Test/Test/logic/LogicTest.java
@@ -0,0 +1,23 @@
+package Test.logic;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class LogicTest
+{
+
+    @Before
+    public void setUp()
+    {
+    }
+
+    @Test
+    public void encrypt()
+    {
+    }
+
+    @Test
+    public void decrypt()
+    {
+    }
+}
